eabc2261e2d04bcbcda82341179d0b2e986f3bc0
Miaru3d <juanjmendoza@protonmail.com>
2025-03-01 23:49:02 -0600

    preparing to make a UI

--- main.py
+++ main.py
@@ -1,35 +1,28 @@
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
-from PIL import ImageChops
 import math, sys, random, time
 
-WATERMARK_OPACITY = 0.1
 
-AMOUNT_FACTOR = 10
-FONT_FACTOR = 90
-FONT_NAME = "arial.ttf"
-WATERMARK_ANGLE = -10
-DEBUG=True
-WATERMARK_SCALE_IMAGE=2
-MIN_FONT_SIZE=9
+class WatermarkConfig:
 
-def guess_font_size(inputImage: Image) -> int:
-    s=max(MIN_FONT_SIZE, math.floor(inputImage.size[1] / FONT_FACTOR))
-    print("Font Size: ",s)
-    return s
-
-
-def guess_amount_watermarks(inputImage: Image) -> int:
-    a=math.floor(inputImage.size[1]*WATERMARK_SCALE_IMAGE / AMOUNT_FACTOR)
-    print("Amount: ",a)
-    return a
+    def __init__(self):
+        self.WATERMARK_OPACITY = 0.1
+        self.DENSITY = 90
+        self.FONT_SIZE_FACTOR = 90
+        self.FONT_NAME = "arial.ttf"
+        self.FONT_COLOR = (255, 255, 255)
+        self.WATERMARK_ANGLE = -10
+        self.DEBUG = True
+        self.WATERMARK_SCALE_IMAGE = 2
+        self.MIN_FONT_SIZE = 9
 
 
-def get_watermarks_positions(inputImage: Image, number_marks: int) -> list:
+def get_watermarks_positions(inputImage: Image, number_marks: int, scale_multiplier) -> list:
     w, h = inputImage.size
     squareRoot = math.floor(math.sqrt(number_marks))
-    blockSize_x, blockSize_y = math.floor(w * WATERMARK_SCALE_IMAGE / squareRoot), math.floor(h* WATERMARK_SCALE_IMAGE/ squareRoot)
+    blockSize_x = math.floor(w * scale_multiplier / squareRoot)
+    blockSize_y = math.floor(h * scale_multiplier / squareRoot)
     squares = []
     centers = []
     for i in range(squareRoot):
@@ -37,60 +30,78 @@ def get_watermarks_positions(inputImage: Image, number_marks: int) -> list:
             squares.append([i * blockSize_x, j * blockSize_y])
     for s in range(number_marks):
         try:
-            centers.append([squares[s][0] + (blockSize_x / random.randrange(1,2)), squares[s][1] + (blockSize_y /random.randrange(1,4))])
+            c_x = squares[s][0] + (blockSize_x / random.randrange(1, 2))
+            c_y = squares[s][1] + (blockSize_y / random.randrange(1, 4))
+            centers.append([c_x, c_y])
         except Exception:
             pass
     return centers
 
 
-def create_watermarks(text: str, size_x: int, size_y: int, centers: list, fontName: str, text_font_size: int,
-                      color=(255, 255, 255), opacity=0.2):
-    empty_watermark_image = Image.new("RGBA", (size_x * WATERMARK_SCALE_IMAGE, size_y * WATERMARK_SCALE_IMAGE), (0, 0, 0, 0))
+def draw_temp_watermark(text: str, size_x: int, size_y: int, centers: list, fontName: str, text_font_size: int,
+                        color=(255, 255, 255), opacity=0.2, scale_multiplier=2):
+    empty_watermark_image = Image.new("RGBA", (size_x * scale_multiplier, size_y * scale_multiplier), (0, 0, 0, 0))
     draw = ImageDraw.Draw(empty_watermark_image, "RGBA")
     font_object = ImageFont.truetype(fontName, text_font_size)
     opacity_hex = int(opacity * 255)
     for c in centers:
-        if random.choice((True,False, True, True)):
-            draw.text(c, text, font=font_object, fill=(color[0], color[1], color[2], opacity_hex),)
+        if random.choice((True, False, True, True)):
+            draw.text(c, text, font=font_object, fill=(color[0], color[1], color[2], opacity_hex), )
         else:
-            t= time.strftime("%d%m%y%H%M")
-            draw.text(c, t, font=font_object, fill=(color[0], color[1], color[2], opacity_hex),font_size=text_font_size)
+            t = time.strftime("%d%m%y%H%M")
+            draw.text(c, t, font=font_object, fill=(color[0], color[1], color[2], opacity_hex),
+                      font_size=text_font_size)
 
     empty_watermark_image.save("temp_watermark.png", "PNG")
     return empty_watermark_image
 
 
-def blend_watermark(original: Image, watermark: Image) -> Image:
+def blend_watermark(original: Image, watermark: Image, scale_multiplier) -> Image:
     copy = original.copy()
-    rotated = watermark.rotate(WATERMARK_ANGLE, resample=Image.Resampling.BICUBIC, expand=True)
-    rotated.save("rotated.png","PNG")
-    pad=copy.size[0]*2/(WATERMARK_SCALE_IMAGE)
-    croped= rotated.crop((pad,pad,copy.size[0] +pad ,copy.size[1] + pad))
+    rotated = watermark.rotate(scale_multiplier, resample=Image.Resampling.BICUBIC, expand=True)
+    rotated.save("rotated.png", "PNG")
+    pad = copy.size[0] * 2 / scale_multiplier
+    croped = rotated.crop((pad, pad, copy.size[0] + pad, copy.size[1] + pad))
     croped.save("croped.png", "PNG")
     return Image.alpha_composite(copy, croped)
 
 
+def make_watermark(file_name: str, text: str, options: WatermarkConfig) -> Image:
+    input_img = Image.open(file_name).convert("RGBA")
+
+    # FONT SIZE
+    fontSize = max(options.MIN_FONT_SIZE, math.floor(input_img.size[1] / options.FONT_SIZE_FACTOR))
+    print("Font Size: ", fontSize)
+
+    # Amount of watermarks
+    amount = math.floor(input_img.size[1] * options.WATERMARK_SCALE_IMAGE / options.DENSITY)
+    print("Amount: ", amount)
+
+    watermarks_centers = get_watermarks_positions(input_img, options.DENSITY, options.WATERMARK_SCALE_IMAGE)
+    blank_watermark = draw_temp_watermark(text, input_img.size[0], input_img.size[1], watermarks_centers,
+                                          options.FONT_NAME,
+                                          fontSize, options.FONT_COLOR, options.WATERMARK_OPACITY)
+    final = blend_watermark(input_img, blank_watermark, options.WATERMARK_SCALE_IMAGE)
+    new_name = file_name.split(".")[0] + "_" + textToPut + ".png"
+    final.save(new_name, "PNG")
+    return final
+
+
 if __name__ == '__main__':
-    if len(sys.argv)==1:
+    if len(sys.argv) == 1:
         print("Debug Run")
-        file_name= "pexels-uriel-venegas-176524868-15321479.jpg"
-        textToPut="SampleText"
+        file_name = "pexels-uriel-venegas-176524868-15321479.jpg"
+        textToPut = "SampleText"
     else:
         try:
             file_name = sys.argv[1]
+            print("Arguments")
         except IndexError:
             print("No input file, quitting")
             quit(0)
         textToPut = input('Text: ')
-        WATERMARK_OPACITY= float(input("Opacity 0.01-1.0 :"))
-
-    inputImg = Image.open(file_name).convert("RGBA")
-    fontSize = guess_font_size(inputImg)
-    amount = guess_amount_watermarks(inputImg)
-
-    watermarks_centers = get_watermarks_positions(inputImg, amount)
-    watermarked_Image = create_watermarks(textToPut, inputImg.size[0], inputImg.size[1], watermarks_centers, FONT_NAME,
-                                          fontSize, opacity=WATERMARK_OPACITY)
-    final = blend_watermark(inputImg, watermarked_Image)
-    new_name = file_name.split(".")[0] + "_" + textToPut + ".png"
-    final.save(new_name, "PNG")
+    options = WatermarkConfig()
+    options.WATERMARK_OPACITY = float(input("Opacity 0.01-1.0 :"))
+    options.DENSITY = int(input("Density 10-100 :"))
+    options.FONT_SIZE_FACTOR = int(input("Size 10-100 :"))
+    make_watermark(file_name, textToPut, options)
