11a393e3795345add3d4a9c9825aeae707be9929
Miaru3d <juanjmendoza@protonmail.com>
2025-03-02 23:10:38 -0600

    GUI works

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import sys
+
+import watermarker
 from watermarker import *
 import json
+import matplotlib.font_manager
 from options_ui import *
 from PySide6.QtWidgets import QColorDialog
 
@@ -17,18 +21,41 @@ class AppUI(QMainWindow):
         if IS_DEBUG:
             self.ui.file_name_edit.setText(DEBUG_IMAGE)
             self.ui.text_edit.setText(DEBUG_TEXT)
+        else:
+            self.ui.file_name_edit.setText(sys.argv[1])
+            self.ui.text_edit.setText("YOUR TEXT HERE")
         #signals
         self.ui.button_box.accepted.connect(self._on_accepted)
+        self.ui.button_box.rejected.connect(sys.exit)
+        ########
+        self.ui.opacity_slider.valueChanged.connect(self._on_opacity_slider_changed)
+        self.ui.opacity_edit.editingFinished.connect(self._on_opacity_text_changed)
+        self._on_opacity_slider_changed()
+        ########
         self.ui.density_slider.valueChanged.connect(self._on_density_slider_changed)
         self._on_density_slider_changed()
         self.ui.density_edit.editingFinished.connect(self._on_density_text_changed)
-
+        #########
         self.ui.color_pick_button.clicked.connect(self._on_pick_color_pressed)
-
+        #########
         self.ui.angle_slider.valueChanged.connect(self._on_angle_changed)
         self.ui.angle_edit.editingFinished.connect(self._on_angle_text_changed)
         self._on_angle_changed()
-
+        #######
+        self.font_ttf_path = ""
+        self.ui.font_selector.currentFontChanged.connect(self._on_font_changed)
+        self._on_font_changed()
+    def _on_font_changed(self):
+        self.ui.text_edit.setFont(self.ui.font_selector.currentFont())
+        _family=str(self.ui.font_selector.currentFont().family())
+        font_prop = matplotlib.font_manager.FontProperties(family=_family)
+        self.font_ttf_path = matplotlib.font_manager.findfont(font_prop)
+        print(self.font_ttf_path)
+    def _on_opacity_text_changed(self):
+        v=int(float(self.ui.opacity_edit.text())*100)
+        self.ui.opacity_slider.setValue(v)
+    def _on_opacity_slider_changed(self):
+        self.ui.opacity_edit.setText(str(self.ui.opacity_slider.value()/100))
     def _on_angle_text_changed(self):
         self.ui.angle_slider.setValue(int(self.ui.angle_edit.text()))
     def _on_density_text_changed(self):
@@ -41,16 +68,23 @@ class AppUI(QMainWindow):
         r,g,b = hex_color[0],hex_color[1],hex_color[2]
         color_text= str(r).rjust(3,"0")+str(g).rjust(3,"0")+str(b).rjust(3,"0")
         self.ui.color_edit.setText(color_text)
+        rgb_as_text= str((r,g,b)) #"color: rgb(255, 0, 0);"
+        self.ui.text_edit.setStyleSheet("color: rgb"+ rgb_as_text +";")
         self.options.FONT_COLOR= (r,g,b)
     def _on_density_slider_changed(self):
         self.ui.density_edit.setText(str(self.ui.density_slider.value()))
     def _on_accepted(self):
         print("Acepted")
-        self.options.WATERMARK_OPACITY= float(self.ui.opacity_spinbox.value())
-        self.options.FONT_SCALE= int(self.ui.font_scale_spinbox.value())
-        self.options.FONT_NAME= self.ui.font_selector.currentFont().family().lower() +".ttf"
-        self.options.DENSITY= self.ui.density_slider.value()
+        self._update_all_options()
+        print(self.options)
+        watermarker.make_watermark(self.ui.file_name_edit.text(),self.ui.text_edit.text(),options=self.options)
 
+    def _update_all_options(self):
+        self.options.WATERMARK_OPACITY = float(self.ui.opacity_edit.text())
+        self.options.FONT_SCALE = int(self.ui.font_scale_spinbox.value())
+        self.options.FONT_PATH = self.font_ttf_path
+        self.options.DENSITY = self.ui.density_slider.value()
+        self.options.WATERMARK_ANGLE = self.ui.angle_slider.value()
 
 
 if __name__ == '__main__':
